8df7b06d9f813a8950e8a14db023a2a7f4462cb4
Walker Ray <walkerdray@gmail.com>
2018-09-01 11:13:52 -0700

    uploads

---
+++ update_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# update files for website
+
+
+# JMP
+prefpol_pdf='/Users/Walker/Dropbox/res/projects/prefpol/tex/output/draft.pdf'
+
+# CV
+CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
+
+# tauction
+tauction_pdf='/Users/Walker/Dropbox/res/projects/Treasury_Auction/tauction/draft/AER-submission/tauction.pdf'
+
+#cp $prefpol_pdf ./pdfs/prefpol.pdf
+cp $CV_pdf ./pdfs/CV.pdf
+cp $tauction_pdf ./pdfs/tauction.pdf
+
+
+
+

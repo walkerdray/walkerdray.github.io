15f7d1af955ce4825cc38b6a5fa4d63a1b7b44ae
Walker Ray <walkerdray@gmail.com>
2018-09-02 17:40:23 -0700

    uploads

--- update_files.sh
+++ update_files.sh
@@ -12,10 +12,15 @@ CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
 # tauction
 tauction_pdf='/Users/Walker/Dropbox/res/projects/Treasury_Auction/tauction/draft/AER-submission/tauction.pdf'
 
-#cp $prefpol_pdf ./pdfs/prefpol.pdf
-cp $CV_pdf ./pdfs/CV.pdf
-cp $tauction_pdf ./pdfs/tauction.pdf
+cp $prefpol_pdf ./pdfs/prefpol.pdf
+#cp $CV_pdf ./pdfs/CV.pdf
+#cp $tauction_pdf ./pdfs/tauction.pdf
 
+
+# update github
+git add -A
+git commit -m "uploads"
+git push -u origin master
 
 
 

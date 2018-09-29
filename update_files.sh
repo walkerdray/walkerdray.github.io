a5719f744104f905ea1f025e8664ec868f2ccc78
Walker Ray <walkerdray@gmail.com>
2018-09-29 10:36:52 -0700

    uploads

--- update_files.sh
+++ update_files.sh
@@ -7,20 +7,23 @@
 prefpol_pdf='/Users/Walker/Dropbox/res/projects/prefpol/tex/output/draft.pdf'
 
 # CV
-CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
+#CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
+CV_pdf='/Users/Walker/Dropbox/res/CV/20180912_CV_Ray.pdf'
 
 # tauction
 tauction_pdf='/Users/Walker/Dropbox/res/projects/Treasury_Auction/tauction/draft/AER-submission/tauction.pdf'
 
-cp $prefpol_pdf ./pdfs/prefpol.pdf
-#cp $CV_pdf ./pdfs/CV.pdf
+echo "copying files"
+#cp $prefpol_pdf ./pdfs/prefpol.pdf
+cp $CV_pdf ./pdfs/CV.pdf
 #cp $tauction_pdf ./pdfs/tauction.pdf
 
-
 # update github
-# git add -A
-# git commit -m "uploads"
-# git push -u origin master
+sleep 1
+echo "uploading"
+git add -A
+git commit -m "uploads"
+git push -u origin master
 
 
 

fd608799e1a9266e473d27f4b7b4f90f4d61dc59
Walker Ray <walkerdray@gmail.com>
2018-09-29 10:45:23 -0700

    uploads

---
+++ copy_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# copy files for website
+
+
+# JMP
+prefpol_pdf='/Users/Walker/Dropbox/res/projects/prefpol/tex/output/draft.pdf'
+
+# CV
+#CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
+CV_pdf='/Users/Walker/Dropbox/res/CV/20180912_CV_Ray.pdf'
+
+# tauction
+tauction_pdf='/Users/Walker/Dropbox/res/projects/Treasury_Auction/tauction/draft/AER-submission/tauction.pdf'
+
+echo "copying files"
+#cp $prefpol_pdf ./pdfs/prefpol.pdf
+cp $CV_pdf ./pdfs/CV.pdf
+#cp $tauction_pdf ./pdfs/tauction.pdf
+
+

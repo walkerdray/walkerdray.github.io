9d5da2b2d8c6500ba19e59247a58367cfce4fe92
Walker Ray <walkerdray@gmail.com>
2018-10-20 23:30:02 -0700

    uploads

--- copy_files.sh
+++ copy_files.sh
@@ -7,14 +7,13 @@
 prefpol_pdf='/Users/Walker/Dropbox/res/projects/prefpol/tex/output/draft.pdf'
 
 # CV
-#CV_pdf='/Users/Walker/Dropbox/res/CV/20180901_resume.pdf'
-CV_pdf='/Users/Walker/Dropbox/res/CV/20180912_CV_Ray.pdf'
+CV_pdf='/Users/Walker/Dropbox/res/CV/20181020_CV_Ray.pdf'
 
 # tauction
 tauction_pdf='/Users/Walker/Dropbox/res/projects/Treasury_Auction/tauction/draft/AER-submission/tauction.pdf'
 
 echo "copying files"
-#cp $prefpol_pdf ./pdfs/prefpol.pdf
+cp $prefpol_pdf ./pdfs/prefpol.pdf
 cp $CV_pdf ./pdfs/CV.pdf
 #cp $tauction_pdf ./pdfs/tauction.pdf
 
